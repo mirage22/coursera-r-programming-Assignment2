ae751d4c2ec1a78c7b87e551d56632af1eb61d2a
Miroslav Kopecky <kopeckymiroslav@gmail.com>
2014-06-22 18:55:47 +0200

    comments improvement

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## the function makeCacheMatrix cointains 4 methods that allows to deal with Inverse matrix. 
-## by calling set function the inverse matrix I is erased for new cached value.
+## the function makeCacheMatrix cointains 4 methods. Those methods allow to deal with matrix x, store inverse matrix I and cache it. 
+## By calling the set function the inverse matrix I is erased and after calculation replace by new  value.
 makeCacheMatrix <- function(x = matrix()){
         
-        ## init inverser property
+        ## init inverse property
         I <- NULL
         
         ## set the matrix x
@@ -35,7 +35,7 @@ makeCacheMatrix <- function(x = matrix()){
 
 
 ## function calculate a inversion matrix of the 'special' matrix CM. The matrix CM is returned by function makeCacheMatrix().
-## If the inverse matrix CM has not been calculated yet or changed, than the inverse matrix IM of the matrix CM is calculated and set 
+## If the inverse matrix CM has not been calculated yet or changed, then the inverse matrix IM of the matrix CM is calculated and set 
 ## and returned as the result. If the matrix CM has been already calculated then it's retrived from the cached valued
 cacheSolve <- function( x, ...){
         ## Return a matrix that is the inverse of 'x'
@@ -43,6 +43,8 @@ cacheSolve <- function( x, ...){
         ## the inversion matrix IM of the matrix x
         IM <- x$getInv()
         
+        ## calculation of the inversion matrix IM (haven't been calculated)
+        ## or has been changed
         if (is.null(IM)) {
                 message('Calculation the inverse...')
                 
